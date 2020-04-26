279b68719f134531ff599b28713753e2716c2e52
Chetan <chetan.vanakudre29@gmail.com>
2020-04-26 21:09:30 +0530

    Console error Fixes

--- src/app/contactForm/contactList/contact-list.component.ts
+++ src/app/contactForm/contactList/contact-list.component.ts
@@ -132,4 +132,4 @@ export class ContactListComponent implements OnInit, OnDestroy {
     return;
 
   }
-}
+} 
--- src/app/contactForm/updateForm/update-form.component.ts
+++ src/app/contactForm/updateForm/update-form.component.ts
@@ -98,3 +98,4 @@ export class UpdateFormComponent implements OnInit, OnChanges {
     }
 
 }
+
